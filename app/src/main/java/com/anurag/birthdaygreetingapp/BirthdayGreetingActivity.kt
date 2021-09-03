68b7a6a1a4de80792535085a8a4898353f76507c
Anurag Pathak <anuragpathak911@gmail.com>
2021-09-03 09:36:30 +0530

    Moving from one activity to another.

--- app/src/main/java/com/anurag/birthdaygreetingapp/BirthdayGreetingActivity.kt
+++ app/src/main/java/com/anurag/birthdaygreetingapp/BirthdayGreetingActivity.kt
@@ -2,10 +2,24 @@ package com.anurag.birthdaygreetingapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.anurag.birthdaygreetingapp.databinding.ActivityBirthdayGreetingBinding
 
 class BirthdayGreetingActivity : AppCompatActivity() {
+    companion object {
+        const val NAME_EXTRA = "name"
+    }
+
+    private lateinit var binding: ActivityBirthdayGreetingBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_birthday_greeting)
+
+        binding = ActivityBirthdayGreetingBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        val name = intent.getStringExtra(NAME_EXTRA)
+        val greet = "Happy Birthday $name"
+        binding.greetingMessage.text = greet
     }
 }
--- app/src/main/java/com/anurag/birthdaygreetingapp/MainActivity.kt
+++ app/src/main/java/com/anurag/birthdaygreetingapp/MainActivity.kt
@@ -1,10 +1,11 @@
 package com.anurag.birthdaygreetingapp
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.View
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.anurag.birthdaygreetingapp.databinding.ActivityMainBinding
+
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
@@ -15,9 +16,11 @@ class MainActivity : AppCompatActivity() {
         val view = binding.root
         setContentView(view)
     }
-    val view = binding.root
+
     fun createBirthCard(view: View) {
         val name = binding.personName.text.toString()
-        Toast.makeText(this, "The name is $name", Toast.LENGTH_LONG).show()
+        val intent = Intent(this, BirthdayGreetingActivity::class.java)
+        intent.putExtra(BirthdayGreetingActivity.NAME_EXTRA, name)
+        startActivity(intent)
     }
 }
