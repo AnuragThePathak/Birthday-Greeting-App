0e0dc566ef8879d35d380910ccce844774cec6e1
Anurag Pathak <anuragpathak911@gmail.com>
2021-09-04 11:13:06 +0530

    Reformatted  code and modified position of text.

--- app/src/main/java/com/anurag/birthdaygreetingapp/BirthdayGreetingActivity.kt
+++ app/src/main/java/com/anurag/birthdaygreetingapp/BirthdayGreetingActivity.kt
@@ -1,7 +1,7 @@
 package com.anurag.birthdaygreetingapp
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import com.anurag.birthdaygreetingapp.databinding.ActivityBirthdayGreetingBinding
 
 class BirthdayGreetingActivity : AppCompatActivity() {
@@ -13,11 +13,13 @@ class BirthdayGreetingActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
         binding = ActivityBirthdayGreetingBinding.inflate(layoutInflater)
+
         val view = binding.root
         setContentView(view)
 
+//        Next lines can be added before setContentView as well but might not
+//        be good
         val name = intent.getStringExtra(NAME_EXTRA)
         val greet = "Happy Birthday\n${name}!"
         binding.greetingMessage.text = greet
